0c99e2bdf48663448dcb2919ccc6cab472498c27
divino-richard <dchard420@gmail.com>
2024-02-06 22:45:04 +0800

    Create login app screen

--- mobile-app/app/(auth)/login.tsx
+++ mobile-app/app/(auth)/login.tsx
@@ -1,9 +1,40 @@
 import { Text, View } from '@/components/Themed';
+import { Image, TextInput, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useState } from 'react';
 
 export default function TabOneScreen() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <View style={{}}>
-      <Text>Login</Text>
+    <View className="flex-1">
+      <View className='items-center py-12 mt-5'>
+        <Image 
+          className="h-[200px] w-[200px]"
+          source={require("../../assets/images/quick-bites-logo.png")}/>
+      </View>
+      <View className='px-5 gap-y-5'>
+        <Text className='font-bold text-sm text-sky-900'>Login Account</Text>
+        
+        <View className='p-3 rounded-lg border border-slate-500'>
+          <TextInput className='text-lg' placeholder="email"/>
+        </View>
+
+        <View className='flex-row justify-between items-center p-3 rounded-lg border border-slate-500'>
+          <TextInput secureTextEntry={showPassword ? false : true} placeholder='password'/>
+          <TouchableOpacity 
+            onPress={() => setShowPassword(!showPassword)}
+          >
+            <Text className='text-slate-500'>
+              <Feather name={showPassword ? "eye" : "eye-off"} size={25} />
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity className='p-4 bg-rose-600 rounded-2xl'>
+          <Text className='text-center text-white font-bold text-sm'>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
---
+++ mobile-app/components/InputText.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from '@/components/Themed';
+import { ReactNode } from 'react';
+import { Image, TextInput } from 'react-native';
+
+interface Props {
+    iconLeft?: ReactNode;
+    placeholder: string;
+    iconRight?: ReactNode;
+    textValue: string;
+    type: string;
+}
+
+export default function InputText(props: Props) {
+  return (
+    <View className="flex-1">
+        
+    </View>
+  );
+}
+
