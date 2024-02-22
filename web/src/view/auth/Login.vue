9cd8c80eb2575867934634bd22e429893d53d1ab
Richard Divino <richarddivino@Richards-Mac-mini.local>
2024-02-22 18:11:55 +0800

    Set up vue js for web

---
+++ web/src/view/auth/Login.vue
@@ -0,0 +1,15 @@
+
+<script setup lang="ts">
+    import { Input } from '../../components/ui/input'
+</script>
+
+<template>
+    <div>
+        <h1>Login</h1>
+
+        <div>
+            <Input placeholder="email"/>
+            <Input placeholder="password"/>
+        </div>
+    </div>
+</template>
