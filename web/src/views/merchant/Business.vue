4fa007633aef9fb53927c1d2fa28dbeb14b32eaf
divino-richard <dchard420@gmail.com>
2024-03-17 11:32:24 +0800

    Display merchant business info

--- web/src/views/merchant/Business.vue
+++ web/src/views/merchant/Business.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { Avatar } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
 import { useStore } from "@/store";
+import { Hash, MapPin } from "lucide-vue-next";
 import { computed, onMounted } from "vue";
 
 const store = useStore();
@@ -18,7 +21,29 @@ onMounted(() => {
 
     <div v-else>
       <div v-if="business">
-        {{ business }}
+        <div class="w-full max-w-[900px] flex gap-x-2 py-5 m-auto">
+          <Avatar class="w-[75px] h-[75px]">
+            <AvataFallback class="text-[35px]">
+              {{ business.name[0].toUpperCase() }}
+            </AvataFallback>
+          </Avatar>
+          <div class="p-2">
+            <h1 class="font-semibold text-[18px] text-zinc-900">{{ business.name }}</h1>
+            <div class="flex items-center gap-x-2 text-[12px] text-zinc-900 font-normal">
+              <MapPin :size="16" />
+              <p>{{ business.address }}</p>
+            </div>
+            <div class="flex items-center gap-x-2 text-[12px] font-normal">
+              <Hash :size="16" />
+              <p>{{ `Registration number: ${business.registrationNumber}` }}</p>
+            </div>
+            <div class="flex items-center gap-x-2 text-[12px] font-normal">
+              <Hash :size="16" />
+              <p>{{ `TIN: ${business.taxIdNumber}` }}</p>
+            </div>
+          </div>
+        </div>
+        <Separator class="w-full max-w-[900px] m-auto bg-zinc-100" />
       </div>
 
       <div
